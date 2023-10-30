556f6e9982e995c0f38ff69e958662d840d2421d
maikoncorrea <maikonacorrea@gmail.com>
2023-10-30 20:31:03 -0300

    app.js configuration

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
-const {PORT = 3000} = process.env;
-
+const { PORT = 3000 } = process.env;
 const app = express();
 
-app.get('/cards', (req, res) => {
-  
-})
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Rota não encontrada');
+  error.status = 404;
+  next(error);
+});
 
 app.listen(PORT, () => {
-  console.log(`App está funcionando na porta ${PORT}`)
+  console.log(`Server running on port: ${PORT}`);
 });
