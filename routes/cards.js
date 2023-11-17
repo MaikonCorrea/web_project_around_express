7cdcc0fd549d397b2609798c814e3dc409291873
maikoncorrea <maikonacorrea@gmail.com>
2023-11-17 16:58:14 -0300

    reconfigured routes/cards post and get

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
+const { listCards, createCard } = require('../controllers/cards');
 
-router.get('/', (req, res) => {
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      res
-        .status(500)
-        .json({ message: 'Erro na leitura do arquivo de cartões' });
-      return;
-    }
-    const cards = JSON.parse(data);
-    res.status(200).json(cards);
-  });
+router.get('/', async (req, res) => {
+  const cards = await listCards();
+  res.json(cards);
+});
+
+router.post('/', async (req, res) => {
+  const { body } = req;
+  const newCard = await createCard(body, req);
+  res.status(201).json(newCard);
 });
 
 module.exports = router;
