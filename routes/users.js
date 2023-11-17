e51ec0e0017ae4614aadad9277501483fa4a0a40
maikoncorrea <maikonacorrea@gmail.com>
2023-11-17 16:58:45 -0300

    reconfigurado routes/users post e get

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,29 @@
 const router = require('express').Router();
-const { listUsers, createUser } = require('../controllers/users')
+const { listUsers, createUser } = require('../controllers/users');
 
 router.get('/', async (req, res) => {
-  const users = await listUsers()
-  res.json(users)
- });
+  const users = await listUsers();
+  res.json(users);
+});
 
- router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const users = await listUsers();
-    const user = users.find(u => u._id.toString() === req.params.id);
-    console.log(user)
+    const user = users.find((u) => u._id.toString() === req.params.id);
 
     if (!user) {
       return res.status(404).json({ error: 'Usuário não encontrado' });
     }
-    res.json(user);
+    return res.json(user);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
+    return res.status(500).json({ error: 'Erro interno do servidor' });
   }
 });
 
 router.post('/', async (req, res) => {
   const { body } = req;
-  const newUser = await createUser(body)
-  res.status(201).json(newUser)
- });
-
+  const newUser = await createUser(body);
+  res.status(201).json(newUser);
+});
 
 module.exports = router;
