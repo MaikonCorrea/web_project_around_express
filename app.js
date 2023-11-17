42d91167906a99327504a265121c2c2e9dcd7e75
maikoncorrea <maikonacorrea@gmail.com>
2023-11-17 00:32:14 -0300

    added bodyParse.json()

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-
+const bodyParser = require('body-parser');
 const { PORT = 3000 } = process.env;
-const app = express();
+const connectDatabase = require('./data/database');
 
-mongoose.connect('mongodb://localhost:27017/aroundb', {
-  useNewUrlParser: true,
-});
+const app = express();
+connectDatabase();
+
+app.use(bodyParser.json());
+
+
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.use('/', (req, res, next) => {
   res.status(404).json({ message: 'A solicitação não foi encontrada' });
-  next(error);
+  next();
 });
 
 app.listen(PORT, () => {
