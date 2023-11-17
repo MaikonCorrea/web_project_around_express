95744c087db874c9ed00720c7160d3b96cc403d1
maikoncorrea <maikonacorrea@gmail.com>
2023-11-17 00:26:06 -0300

    updated get/post routes

--- routes/users.js
+++ routes/users.js
@@ -1,72 +1,32 @@
-const router = require('express').Router;
-const fs = require('fs').promises;
-const path = require('path');
-
-const file = path.join(__dirname, '..', 'data', 'users.json');
+const router = require('express').Router();
+const { listUsers, createUser } = require('../controllers/users')
 
 router.get('/', async (req, res) => {
-  try {
-    const data = await fs.readFile(file);
-    const users = JSON.parse(data);
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'erro na leitura de arquivos do usuário' });
-  }
-});
+  const users = await listUsers()
+  res.json(users)
+ });
 
-router.get('/:id', async (req, res) => {
+ router.get('/:id', async (req, res) => {
   try {
-    const data = await fs.readFile(file);
-    const users = JSON.parse(data);
-    const userId = req.params.id;
-    const user = users.find(() => user._id === userId);
+    const users = await listUsers();
+    const user = users.find(u => u._id.toString() === req.params.id);
+    console.log(user)
 
     if (!user) {
-      res.status(404).json({ message: 'ID do usuário não encontrado' });
-      return;
+      return res.status(404).json({ error: 'Usuário não encontrado' });
     }
-    res.status(200).json(user);
+    res.json(user);
   } catch (error) {
-    res.status(500).json({ message: 'Erro ao ler usuário' });
+    console.error(error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
   }
 });
 
-/* router.post('/users', async (req, res) => {
-  try {
-    const data = await fs.readFile(file);
-    const users = JSON.parse(data);
+router.post('/', async (req, res) => {
+  const { body } = req;
+  const newUser = await createUser(body)
+  res.status(201).json(newUser)
+ });
 
-    const newUser = req.body; // Supondo que o corpo da requisição contenha as informações do novo usuário
-    users.push(newUser);
 
-    await fs.writeFile(file, JSON.stringify(users, null, 2));
-
-    res.status(201).json(newUser); // Retorna o novo usuário criado
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao adicionar novo usuário" });
-  }
-}); */
-
-/* router.post('/users', async (req, res) => {
-  try {
-    const data = await fs.readFile(file);
-    const users = JSON.parse(data);
-
-    const { name, about, avatar } = req.body;
-
-    const newUser = {
-      _id: generateUniqueId(), // Você pode criar uma função para gerar IDs únicos
-      name,
-      about,
-      avatar
-    };
-
-    users.push(newUser);
-
-    await fs.writeFile(file, JSON.stringify(users, null, 2));
-
-    res.status(201).json(newUser); // Retorna o novo usuário criado
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao adicionar novo usuário" });
-  }
-}); */
+module.exports = router;
