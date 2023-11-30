03d3cb532df4729ac8a39c23ebd4a4d480b99847
maikoncorrea <maikonacorrea@gmail.com>
2023-11-29 21:37:37 -0300

    update card routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,15 +1,53 @@
 const router = require('express').Router();
-const { listCards, createCard } = require('../controllers/cards');
+const { listCards, createCard, deleteCard } = require('../controllers/cards');
+const CustomError = require('../errors/CustomError');
 
-router.get('/', async (req, res) => {
-  const cards = await listCards();
-  res.json(cards);
+router.get('/', async (req, res, next) => {
+  try {
+    const cards = await listCards();
+    res.json(cards);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res) => {
   const { body } = req;
-  const newCard = await createCard(body, req);
-  res.status(201).json(newCard);
+  try {
+    const newCard = await createCard(body, req);
+    res.status(200).json(newCard);
+  } catch (error) {
+    res
+      .status(error.statusCode)
+      .json({
+        message: error.message,
+        type: error.name,
+        Status: error.statusCode,
+      });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const cardId = req.params.id;
+
+  try {
+    const cards = await listCards();
+    const cardExists = cards.some((card) => card.id === cardId);
+
+    if (!cardExists) {
+      throw new CustomError('ID não encontrado na lista de cartões', 'Not Found', 404);
+    }
+    const deletedCard = await deleteCard(cardId);
+    res.status(204).send(deletedCard);
+  } catch (error) {
+    res
+      .status(error.statusCode)
+      .json({
+        message: error.message,
+        type: error.name,
+        Status: error.statusCode,
+      });
+  }
 });
 
 module.exports = router;
