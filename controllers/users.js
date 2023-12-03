ab8596288837e670da806c3b44b2c3cd57260e71
maikoncorrea <maikonacorrea@gmail.com>
2023-12-03 12:41:07 -0300

    updated createUser

--- controllers/users.js
+++ controllers/users.js
@@ -24,9 +24,40 @@ module.exports = {
     } catch (error) {
       if (error.name === 'ValidationError') {
         const field = Object.keys(error.errors)[0];
-        const message = error.errors[field].message;
+        const { message } = error.errors[field];
         throw new CustomError(message, 'ValidationError', 400);
       }
+      throw error;
+    }
+  },
+  updateUser: async (userId, updatedData) => {
+    try {
+      if (!userId) {
+        throw new CustomError('ID de usuário inválido!', 'InvalidUserIdError', 400);
+      }
+
+      const existingUser = await User.findById(userId);
+      if (!existingUser) {
+        throw new CustomError('Usuário não encontrado!', 'UserNotFoundError', 404);
+      }
+
+      if (updatedData.avatar) {
+        const urlRegex = /^https?:\/\/[^\s]+$/;
+        const isAvatarValid = updatedData.avatar.match(urlRegex);
+        if (!isAvatarValid) {
+          throw new CustomError('Link do Avatar é inválido!', 'InvalidLinkError', 400);
+        }
+      }
+      Object.assign(existingUser, updatedData);
+      const updatedUser = await existingUser.save();
+      return updatedUser;
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        const field = Object.keys(error.errors)[0];
+        const { message } = error.errors[field];
+        throw new CustomError(message, 'ValidationError', 400);
+      }
+      throw error;
     }
   },
 };
